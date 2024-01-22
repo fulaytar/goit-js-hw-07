0146eef4d097e400267969e6ce7a183667211439
fulaytar <fulaytar2405@gmail.com>
2024-01-23 00:21:58 +0200

    hw 5 donne

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,28 @@
+const registerForm = document.querySelector(".login-form");
+const userAccount = {
+    email: "",
+    password:"",
+}
+function dataVerification(event) {
+    event.preventDefault(); //скасував обновлення сторінки
+    const form = event.target; //беру доступ до форми
+    const email = form.elements.email.value.trim();
+    
+    if (email == "" || email.includes(" ")) {
+    return  alert('All form fields must be filled in')
+    } else {
+        userAccount.email = email;
+    }
+    const password = form.elements.password.value;
+    
+    if (password == "" || password.includes(" ")) {
+        return alert('All form fields must be filled in')
+    } else {
+        userAccount.password = password;
+    }
+    console.log(userAccount);
+    form.reset();
+}
 
+
+registerForm.addEventListener("submit", dataVerification)
--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector(".change-color");
+const spanText = document.querySelector(".color");
+const widgetBlock=document.querySelector(".widget")
+
+function changeColor() {
+  let randomHex = getRandomHexColor();
+  spanText.textContent = randomHex;
+  widgetBlock.style.backgroundColor=`${randomHex}`
+}
+button.addEventListener("click", changeColor);
